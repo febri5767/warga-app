5bcf64929e08bb860757e957451e668b97e7314d
Febri <febriluhramadhan@yahoo.co.id>
2024-06-04 10:05:42 +0700

    modify edit warga, tambah warga

--- resources/views/warga/edit.blade.php
+++ resources/views/warga/edit.blade.php
@@ -6,50 +6,72 @@
         </div>
         <div class="card-body">
             <form action="">
-                <div class="mb-3">
-                    <label for="nama" class="form-label">Nama</label>
-                    <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama">
+                <div class="container">
+                    <div class="mb-3">
+                        <label for="nama" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama">
+                    </div>
+                    <div class="row align-items-start">
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="tempat_lahir" class="form-label">Tempat Lahir</label>
+                                <input type="text" class="form-control" name="tempat_lahir" id="tempat_lahir"
+                                    placeholder="Tempat Lahir">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="tgl_lahir" class="form-label">Tanggal Lahir</label>
+                                <input type="date" class="form-control" name="tgl_lahir" id="tgl_lahir"
+                                    placeholder="Tanggal Lahir">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row align-items-start">
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
+                                <select class="form-select" aria-label="Default select example" name="jenis_kelamin"
+                                    id="jenis_kelamin">
+                                    <option value="1">Laki-laki</option>
+                                    <option value="2">Perempuan</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="status" class="form-label">Status</label>
+                                <select class="form-select" aria-label="Default select example" name="status"
+                                    id="status">
+                                    <option value="1">Belum Kawin</option>
+                                    <option value="2">Kawin</option>
+                                    <option value="3">Cerai Hidup</option>
+                                    <option value="4">Cerai Mati</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="gol_dar" class="form-label">Golongan Darah</label>
+                                <select class="form-select" aria-label="Default select example" name="gol_dar"
+                                    id="gol_dar">
+                                    <option selected>Tidak tahu</option>
+                                    <option value="1">A</option>
+                                    <option value="2">B</option>
+                                    <option value="3">AB</option>
+                                    <option value="4">O</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="pekerjaan" class="form-label">Pekerjaan</label>
+                        <input type="text" class="form-control" name="pekerjaan" id="pekerjaan" placeholder="Pekerjaan">
+                    </div>
+
+                    <button class="btn btn-primary"> <i class="fas fa-save"></i> Simpan</button>
                 </div>
-                <div class="mb-3">
-                    <label for="tempat_lahir" class="form-label">Tempat Lahir</label>
-                    <input type="text" class="form-control" name="tempat_lahir" id="tempat_lahir"
-                        placeholder="Tempat Lahir">
-                </div>
-                <div class="mb-3">
-                    <label for="tgl_lahir" class="form-label">Tanggal Lahir</label>
-                    <input type="date" class="form-control" name="tgl_lahir" id="tgl_lahir" placeholder="Tanggal Lahir">
-                </div>
-                <div class="mb-3">
-                    <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
-                    <select class="form-select" aria-label="Default select example" name="jenis_kelamin" id="jenis_kelamin">
-                        <option value="1">Laki-laki</option>
-                        <option value="2">Perempuan</option>
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label for="status" class="form-label">Status</label>
-                    <select class="form-select" aria-label="Default select example" name="status" id="status">
-                        <option value="1">Belum Kawin</option>
-                        <option value="2">Kawin</option>
-                        <option value="3">Cerai Hidup</option>
-                        <option value="4">Cerai Mati</option>
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label for="pekerjaan" class="form-label">Pekerjaan</label>
-                    <input type="text" class="form-control" name="pekerjaan" id="pekerjaan" placeholder="Pekerjaan">
-                </div>
-                <div class="mb-3">
-                    <label for="gol_dar" class="form-label">Golongan Darah</label>
-                    <select class="form-select" aria-label="Default select example" name="gol_dar" id="gol_dar">
-                        <option selected>Tidak tahu</option>
-                        <option value="1">A</option>
-                        <option value="2">B</option>
-                        <option value="3">AB</option>
-                        <option value="4">O</option>
-                    </select>
-                </div>
-                <button class="btn btn-primary"> <i class="fas fa-save"></i> Simpan</button>
             </form>
         </div>
     </div>
